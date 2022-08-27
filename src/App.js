125d39737f3a39bb38e995dd9fd5489b62fe46bc
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2022-08-27 22:31:36 +0600

    SetupReact

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import React, {useEffect} from 'react';
+import './App.css';
+
+function App() {
+    useEffect(() => {
+      
+    }, []);
+ 
+ 
+  return (
+    <div className="App">
+        <h1>lets create api</h1>
+    </div>
+  );
+}
+
+export default App;
