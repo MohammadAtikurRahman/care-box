867a4c297768bd7a11bc018f332390edcf0f251e
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2022-08-28 13:36:31 +0600

    refactor

--- src/Button.js
+++ src/Button.js
@@ -3,17 +3,30 @@ import React  from 'react'
 
 import './App.css';
 function Button(){
+    
+     function myButton() { 
+	document.getElementById("mainFrameOne").style.display="none"; 
+	document.getElementById("mainFrameTwo").style.display="block"; 
+  }
+
+
 
    return(
+
+    
+
+
+
        <div className='up'>
 
+          
+       
             <button  className="bbtn ssuccess "  >
                         
             <i class="fa fa-spinner fa-spin pst"  ></i>
                         
              Streaming link one</button>
 
-             
               <button  className="bbtn ssuccess "  >
                         
             <i class="fa fa-spinner fa-spin pst"  ></i>
